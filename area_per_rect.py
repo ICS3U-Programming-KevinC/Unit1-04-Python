86a6da5fa2df7a62af679b2a7a0bc8f020fefc4b
Kevin Csiffary <kevin.csiffary@gmail.com>
2022-09-15 21:48:36 +0000

    created basic_math.py and area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,21 @@
+# !/user/bin/env python3
+
+# Created by Kevin Csiffary
+# Date: Sep. 14, 2022
+# This program calculates the area and perimeter of a rectangle
+# With dimention 3cm * 7cm
+
+
+def main():
+    print("if a rectangle has the dimentions:")
+    print("3cm * 7cm")
+    print("")
+    print("Then its area should be:")
+    print(f"{3*7}cm²")
+    print("")
+    print("and its perimeter should be:")
+    print(f"{2*(3+7)}cm")
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,16 @@
+# !/user/bin/env python3
+
+# Created by Kevin Csiffary
+# Date: Sep. 15, 2022
+# This program does basic math
+
+
+def main():
+    print("4+7={}".format(4+7))
+    print("12-5={}".format(12-5))
+    print("24/5={}".format(24/5))
+    print("43*6={}".format(43*6))
+    print("3^4={}".format(3^4))
+
+if __name__ == "__main__":
+    main()
